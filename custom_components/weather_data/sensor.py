7a05149ab1212a8a15191cb3da4acfa50debaf64
Daniel Hjelseth Høyer <mail@dahoiv.net>
2020-09-27 13:07:55 +0200

    update url

--- custom_components/weather_data/sensor.py
+++ custom_components/weather_data/sensor.py
@@ -166,7 +166,7 @@ class WeatherData:
     def __init__(self, hass, coordinates, forecast, devices):
         """Initialize the data object."""
         self._url = (
-            "https://aa015h6buqvih86i1.api.met.no/weatherapi/locationforecast/1.9/"
+            "https://api.met.no/weatherapi/locationforecast/1.9/"
         )
         self._urlparams = coordinates
         self._forecast = forecast
